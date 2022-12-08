332827eae03b7751e0bdb1f21b488c86872e4a6e
sbenitez <sbenitez.js@gmail.com>
2022-12-08 20:25:29 -0300

    arduino

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 
 EthernetConnection ethernetConnection = EthernetConnection();
 
+bool encendido = true;
+
+void (*resetFunc)(void) = 0; // declare reset function @ address 0
+
 void setup()
 {
   Serial.begin(9600);
@@ -20,34 +24,73 @@ void setup()
   }
 }
 
+bool show = false;
+
 void loop()
 {
-  String line = ethernetConnection.readLine();
-  int rand = random(100);
-
-  if(line.length() > 0) {
-    Serial.println(line);
-  }
-  
-  if (line == "ping")
+  if (encendido)
   {
-    ethernetConnection.writeLine("pong");
-    ethernetConnection.maintain();
-  }
 
-  if (line == "hola")
-  {
-    ethernetConnection.writeLine("hola como estais?");
-    ethernetConnection.maintain();
-  }
+    String line = ethernetConnection.readLine();
+    DynamicJsonDocument doc(2048);
 
-  if (rand >= 99)
-  {
-    String num = String(rand);
-    String message = " es el num que salio";
-    num.concat(message);
-    ethernetConnection.writeLine(num);
-    ethernetConnection.maintain();
-  }
+    if (line.length() > 0)
+    {
+      // Serial.println(line);
 
+      deserializeJson(doc, line);
+      String command = doc["command"];
+      Serial.println(command);
+
+      if (command == "null")
+      {
+        Serial.println("JSON Invalido:");
+        Serial.println(line);
+      }
+    
+      else if (command == "ping")
+      {
+        ethernetConnection.writeLine("pong");
+        ethernetConnection.maintain();
+      }
+
+      else if (command == "apagar")
+      {
+        encendido = false;
+        delay(1000); // wait for a second
+        Serial.println("resetting");
+        resetFunc(); // call reset
+      }
+
+      else if (command == "delay")
+      {
+        delay(10000);
+        ethernetConnection.writeLine("pong");
+        ethernetConnection.maintain();
+      }
+      
+      else if (command == "dispatch")
+      {
+        DynamicJsonDocument doc(1024);
+        String result;
+        doc["response"] = "ok";
+        doc["line"] = 1;
+        delay(1000);
+        serializeJson(doc, result);
+        ethernetConnection.writeLine(result);
+
+        delay(7000);
+        result = "";
+        doc["response"] = "ok";
+        doc["line"] = 4;
+        serializeJson(doc, result);
+        ethernetConnection.writeLine(result);
+        // This prints:
+        // {"sensor":"gps","time":1351824120,"data":[48.756080,2.302038]}
+
+        ethernetConnection.maintain();
+      }
+    }
+
+  }
 }
